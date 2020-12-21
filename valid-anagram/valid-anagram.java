5d340556d0c0f90603d901ecb7c3cc1f2a23d87c
Shrinath Joshi <shrinathjoshi1997@gmail.com>
2020-12-22 02:22:05 +0530

    Time: 2 ms (95.18%), Space: 39.2 MB (75.38%) - LeetHub

---
+++ valid-anagram/valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s==null && t==null)
+            return true;
+        if(s==null || t == null || s.length()!=t.length())
+            return false;
+        
+        
+        char sArr[] = s.toCharArray();
+        char tArr[] = t.toCharArray();
+        
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        
+        for(int i=0;i<sArr.length;i++){
+            if(sArr[i]!=tArr[i])
+                return false;
+        }
+        return true;
+    }
+}
